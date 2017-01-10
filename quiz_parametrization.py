60047a993d7cc381c1d51bfdb555e8f2abe1faf0
Slawek Kolodziej <hfrntt@gmail.com>
2017-01-10 10:38:16 +0100

    Add more examples to parametrization quiz

--- quiz_parametrization.py
+++ quiz_parametrization.py
@@ -4,12 +4,34 @@ from plane import Plane
 from vector import Vector
 
 
-s = LinearSystem([
-    Plane(normal_vector=Vector(['0.786','0.786','0.588']), constant_term='-0.714'),
-    Plane(normal_vector=Vector(['-0.138', '-0.138', '0.244']), constant_term='0.319')
-])
+try:
+    s = LinearSystem([
+        Plane(normal_vector=Vector(['0.786','0.786','0.588']), constant_term='-0.714'),
+        Plane(normal_vector=Vector(['-0.138', '-0.138', '0.244']), constant_term='0.319')
+    ])
+    print s.compute_solution()
+except Exception as e:
+    print str(e)
+
 
 try:
+    s = LinearSystem([
+        Plane(normal_vector=Vector(['8.631', '5.112', '-1.816']), constant_term='5.113'),
+        Plane(normal_vector=Vector(['4.315', '11.132', '-5.27']), constant_term='-6.775'),
+        Plane(normal_vector=Vector(['-2.158', '3.01', '-1.7272']), constant_term='-0.831')
+    ])
+    print s.compute_solution()
+except Exception as e:
+    print str(e)
+
+
+try:
+    s = LinearSystem([
+        Plane(normal_vector=Vector(['0.935', '1.76', '-9.365']), constant_term='-9.955'),
+        Plane(normal_vector=Vector(['0.187', '0.352', '-1.873']), constant_term='-1.991'),
+        Plane(normal_vector=Vector(['0.374', '0.704', '-3.746']), constant_term='-3.982'),
+        Plane(normal_vector=Vector(['-0.561', '-1.056', '5.619']), constant_term='5.973')
+    ])
     print s.compute_solution()
 except Exception as e:
     print str(e)
